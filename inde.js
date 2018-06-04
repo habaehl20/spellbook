5c72d8061e05e151d582d1752c3017a957f5648a
Hunter Baehl <habaehl20@wabash.edu>
2018-06-04 14:37:30 -0400

    Change Text button

--- inde.js
+++ inde.js
@@ -1,1 +1,8 @@
 console.log('testing')
+const button = document.querySelector('button')
+function changeH1(){
+    const heading1 = document.querySelector('h1')
+    heading1.textContent = 'The New SpellBook'
+}
+button.addEventListener('click',changeH1)
+
