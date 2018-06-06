86f2533d2ad9a07a882ec307794a1b9c7d988f5c
Hunter Baehl <habaehl20@wabash.edu>
2018-06-06 18:17:19 -0400

    change elixir to level

--- inde.js
+++ inde.js
@@ -22,34 +22,51 @@ const app = {
   const form = document.querySelector('form')
   form.addEventListener('submit', ev => {
   this.handleSubmit(ev)
-  })
+    })
   },
+
   renderProperty: function(name, value) {
   const el = document.createElement('span')
   el.textConcent = value
   el.classList.add(name)
   return el
   },
+
+
   renderItem: function(spell) {
+
   properties = Object.keys(spell)
+
   const childElements = properties.map(property => {
-  return this.renderProperty(property, spell[property])
+    return this.renderProperty(property, spell[property])
   })
+
   const item = document.createElement('li')
   item.classList.add('spell')
+
   childElements.forEach(el => {
-  item.appendChild(el)
+    item.appendChild(el)
   })
-  return item
+
+    return item
+  
   },
+
+
   handleSubmit: function(ev) {
   ev.preventDefault()
+
   const f = ev.target
+
   const spell = {
+
   name: f.spellName.value,
-  elixir: f.elixir.value,
+  level: f.level.value,
+
   }
+
   const item =this.renderItem(spell)
+  
   const list = document.querySelector('#spells') 
   list.appendChild(item)
   f.reset()
